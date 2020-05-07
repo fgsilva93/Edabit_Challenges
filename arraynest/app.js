3fd04f8b0081d7119722a3428ff22d68b767956e
Franco Silva <francosilva@Francos-MacBook-Pro.local>
2020-05-07 16:54:25 -0500

    added more sample

--- arraynest/app.js
+++ arraynest/app.js
@@ -7,4 +7,7 @@ function canNest(arr1, arr2) {
 	}
 }
 
-console.log(canNest([1, 2, 3, 4], [0, 6]))
+console.log(canNest([1, 2, 3, 4], [0, 6]));
+console.log(canNest([3, 1], [4, 0]));
+console.log(canNest([9, 9, 8], [8, 9]));
+console.log(canNest([1, 2, 3, 4], [2, 3]));
