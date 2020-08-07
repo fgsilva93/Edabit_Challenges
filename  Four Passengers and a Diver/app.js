05af40b57ea92b9d01aabcac5d2cdb0be0d39eab
fgsilva93 <fgsilva93@gmail.com>
2020-08-06 23:06:18 -0500

    finish work

---
+++  Four Passengers and a Diver/app.js
@@ -0,0 +1,12 @@
+function carsNeeded(n) {
+	return Math.ceil(n/5);
+}
+
+//testing varibles 
+
+console.log(carsNeeded(0));
+console.log(carsNeeded(1));
+console.log(carsNeeded(4));
+console.log(carsNeeded(5));
+console.log(carsNeeded(6));
+console.log(carsNeeded(18));
