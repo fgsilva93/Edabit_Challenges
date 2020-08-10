c246f59b02393ef968202a66286fdbb237417844
fgsilva93 <fgsilva93@gmail.com>
2020-08-09 21:49:55 -0500

    finish work

---
+++ Return a String as an Integer/app.js
@@ -0,0 +1,9 @@
+function stringInt(str) {
+	return Number(str);
+}
+
+//tesing variables
+console.log(stringInt("6"));
+console.log(stringInt("2"));
+console.log(stringInt("10"));
+console.log(stringInt("666"));
